6f2940f248ff8215c62c0c2f960a657de1c899ea
Brian Nwaijah <brian.nwaijah@elev.ga.ntig.se>
2024-11-14 12:28:57 +0100

    steg 1

---
+++ 23C_Steg 1/Program.cs
@@ -0,0 +1,18 @@
+﻿// if(6 > 3)
+// {
+// Console.WriteLine("Hello, World!");
+// Console.ReadLine();
+// }
+
+// Console.WriteLine("Skriv ditt användarnamn");
+// string användarnamn = Console.ReadLine();
+
+// if( användarnamn == "noname");
+// {
+    // Console.WriteLine("Welcome");
+    // Console.ReadLine();
+
+// }
+
+
+
